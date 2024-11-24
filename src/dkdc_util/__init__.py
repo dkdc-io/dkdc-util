ab8e532f790638c9f9bfba3a2cca16c9e25d416c
Cody <cody@dkdc.dev>
2024-11-24 12:39:52 -0500

    remove load_env utility

--- src/dkdc_util/__init__.py
+++ src/dkdc_util/__init__.py
@@ -1,10 +1,3 @@
-from dkdc_util.main import now, uuid_parts, uuid, uuid_to_parts, get_dkdc_dir, load_env
+from dkdc_util.main import now, uuid_parts, uuid, uuid_to_parts, get_dkdc_dir
 
-__all__ = [
-    "now",
-    "uuid_parts",
-    "uuid",
-    "uuid_to_parts",
-    "get_dkdc_dir",
-    "load_env",
-]
+__all__ = ["now", "uuid_parts", "uuid", "uuid_to_parts", "get_dkdc_dir"]
